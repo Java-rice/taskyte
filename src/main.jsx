f65458b1e6d2abe1447bfb961d4371ed9dd0d24c
Java-rice <johnmark.p.peroche@gmail.com>
2025-06-20 10:34:25 +0800

    feat: setup queryClient

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Root from '@src/services/Root'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      onError: (error) => {
+        if (error.response && error.response.status === 401) {
+          // Handle JWT expiration
+        }
+      },
+    },
+    mutations: {
+      onError: (error) => {
+        if (error.response && error.response.status === 401) {
+          // Handle JWT expiration
+        }
+      },
+    },
+  },
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Root />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition='bottom-right'/>
+    </QueryClientProvider> 
+    
   </StrictMode>,
 )
---
+++ src/services/Root.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import App from '@src/App.jsx'
+const Root = () => {
+  return <App/>
+}
+
+export default Root
