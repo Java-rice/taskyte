cc39c816a3e2ab530a65eff19749418575d35328
Java-rice <johnmark.p.peroche@gmail.com>
2025-06-21 14:56:09 +0800

    feat: added backend directory

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '@src/App.css'
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+      <div>test</div>
+    </>
+  )
+}
+
+export default App
--- client/src/services/Root.jsx
+++ client/src/services/Root.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from '@src/App.jsx'
+
 const Root = () => {
   return <App/>
 }
